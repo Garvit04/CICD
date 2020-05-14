4dda8b7e1639c290ce05755a74cf2274c68f5396
Garvit04 <jhanwar.pul4@gmail.com>
2020-05-14 15:10:03 +0530

    Update Prod.java

--- Prod.java
+++ Prod.java
@@ -13,7 +13,7 @@ public class Prod {
 
 private static final String POST_URL = "https://api.boomi.com/api/rest/v1/traininggarvitmaheshwari-PSQHLZ/DeployedPackage/";
 
-private static final String POST_PARAMS = "{\n" + "\"environmentId\" : \"b296bb79-ed4c-4e45-a7d9-2c67ec38d70a\",\r\n" + "\"packageId\" : \"abb7d77b-8c17-4814-a549-5195c961c339\",\r\n" + "\"notes\" : \"Package deployment via Rest request\"" + "\n}";
+private static final String POST_PARAMS = "{\n" + "\"environmentId\" : \"b296bb79-ed4c-4e45-a7d9-2c67ec38d70a\",\r\n" + "\"packageId\" : \"f37315f0-424d-48d6-9e06-4da747b91404\",\r\n" + "\"notes\" : \"Package deployment via Rest request\"" + "\n}";
 
 public static void main(String[] args) throws IOException {
 
